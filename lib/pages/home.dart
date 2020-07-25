4a5f208527c0e2d0e6ba569e35c9ac1fa3d3c46a
jczzq <1195262383@qq.com>
2020-07-25 20:29:26 +0800

    feat: http request

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body: Center(
         child: Text('Pulse 首页'),
       ),
     );
+  }
+
+  void getHttp() async {
+    try {
+      Response res;
+      res = await Dio().get('https://baidu.com');
+      return print(res);
+    } catch (e) {
+      return print(e);
+    } finally {}
   }
 }
